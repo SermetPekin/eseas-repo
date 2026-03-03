a836c0bd2daa370085bc8b46390f7d1333f80889
Sermet Pekin <sermet.pekin@gmail.com>
2026-03-03 23:44:02 +0300

    j

---
+++ jwsacruncher/jwsacruncher-2.2.5/bin/demetra_commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Starting the script..."
+# airpassengers
+# --------------------------------------------------
+
+/Users/sermetpekin/eseas-repo/jwsacruncher/jwsacruncher-2.2.5/bin/jwsacruncher "/Users/sermetpekin/eseas-repo/eseas_output/@eseas_wspace_/Users/sermetpekin/eseas-repo/eseas/data_for_testing/unix/airpassengers.xml" -x "/Users/sermetpekin/eseas-repo/jwsacruncher/jwsacruncher-2.2.5/bin/general.params" -d "/Users/sermetpekin/eseas-repo/eseas_output/@eseas_wspace_/Users/sermetpekin/eseas-repo/eseas/data_for_testing/unix/test_output/airpassengers"
+
