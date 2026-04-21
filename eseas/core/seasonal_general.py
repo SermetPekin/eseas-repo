af0c514ccd2b290f4aa7f1e136823ad586a997c7
Sermet Pekin <96650846+SermetPekin@users.noreply.github.com>
2026-04-21 10:13:55 +0300

    Update seasonal_general.py

--- eseas/core/seasonal_general.py
+++ eseas/core/seasonal_general.py
@@ -50,13 +50,19 @@ class SeasonalADV:
         out_folder=None,
         out_file_name="combined",
         encoding="latin-1",
-    ):
+        special_names = None 
+    ):  
+        special_names = self.options.special_names if special_names is None else special_names
+        if not special_names is None : 
+            if not isinstance(special_names, (tuple, list)):
+                special_names= [special_names]
         collect(
             self.options,
             xml_folders=xml_folders,
             out_folder=out_folder,
             out_file_name=out_file_name,
             encoding=encoding,
+            special_names = special_names 
         )
 
     def run(self):
@@ -92,10 +98,9 @@ class SeasonalADV:
                 "You may type y next time if you like them to run."
             )
             return
-        
+
         # run_bat_commands()
         # run_bat_commands_direct()
-        
 
     def code_reproduce(self):
         return ReproduceMevsimsel(self).code_reproduce()
@@ -138,5 +143,6 @@ def main():
 main()
         """
         return template
+
 
 __all__ = ["SeasonalADV"]
