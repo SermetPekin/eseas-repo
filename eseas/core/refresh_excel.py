88861f958d6a8d48a95a37ee5a2cd7f190425aac
Sermet Pekin <96650846+SermetPekin@users.noreply.github.com>
2026-04-22 11:54:31 +0300

    Update refresh_excel.py

--- eseas/core/refresh_excel.py
+++ eseas/core/refresh_excel.py
@@ -10,7 +10,7 @@ import subprocess
 if sys.platform == "win32":
     import pythoncom
     import win32com.client
-    
+
 
 def get_input():
     ans = input(
@@ -59,10 +59,19 @@ class File:
 
     """
 
-    def __init__(self, name: str, root: str = None, approve: bool = False, dont_check=False):
+    def __init__(
+        self,
+        name: str,
+        root: str = None,
+        macro_name="Macro1",
+        approve: bool = False,
+        dont_check=False,
+    ):
         self.name = name
         self.root = root
+        self.macro_name = macro_name
         self.dont_check = dont_check
+
         if self.root is None:
             self.path = Path(name).absolute()
         else:
@@ -87,16 +96,28 @@ class File:
         if not self.exists():
 
             raise ValueError(f"File Not found ! {self.path}")
-        assert self.exists()
 
-    def refresh(self, excel_app):  # Pass excel_app as an argument
+    def sleep(self, seconds: 0):
+        import time
+
+        time.sleep(seconds)
+
+    def refresh(self, excel_app, sleep=0):  # Pass excel_app as an argument
         try:
             print(f"working on: {self}")
             wb = excel_app.Workbooks.Open(self.path)
+            for conn in wb.Connections:
+                if conn.Type == 1:  # OLEDB Connection
+                    conn.OLEDBConnection.BackgroundQuery = False
+                elif conn.Type == 2:  # ODBC Connection
+                    conn.ODBCConnection.BackgroundQuery = False
+
             wb.RefreshAll()
-            time.sleep(1)
+
             wb.Save()
+            self.sleep(sleep)
             wb.Close(False)
+
             print(f"Done => {self}")
         except Exception as e:
             import traceback
@@ -105,10 +126,76 @@ class File:
 
             print(f"Error - {self}:\n{e}")
 
+    def refresh_macro(self, excel_app, sleep=0):
+        try:
+            print(f"working on: {self}")
+            wb = excel_app.Workbooks.Open(self.path)
 
-def check_and_get_files(files, root=None, approve: bool = False, dont_check: bool = False):
+            for conn in wb.Connections:
+                if conn.Type == 1:  # OLEDB Connection
+                    conn.OLEDBConnection.BackgroundQuery = False
+                elif conn.Type == 2:  # ODBC Connection
+                    conn.ODBCConnection.BackgroundQuery = False
 
-    fs = [File(x, root=root, approve=approve, dont_check=dont_check) for x in files]
+            wb.RefreshAll()
+
+            excel_app.Application.Run(self.macro_name)
+
+            wb.Save()
+            self.sleep(sleep)
+
+            wb.Close(False)
+            print(f"Done [Macro: {self.macro_name}] => {self}")
+        except Exception as e:
+            import traceback
+
+            traceback.print_exc()
+
+            print(f"Error - {self}:\n{e}")
+
+
+def check_and_get_files(
+    files,
+    macro_names: tuple = None,
+    root=None,
+    approve: bool = False,
+    dont_check: bool = False,
+):
+    if isinstance(files, (str, Path)):
+        files = [files]
+    if isinstance(macro_names, str):
+        if len(files) == 1:
+            macro_names = [macro_names]
+        else:
+            import warnings
+
+            warnings.warn(f"""
+                          
+                          Assuming all macro names are {macro_names}
+                          
+                          if they differ consider giving them as a list 
+                          
+                          >  refresh_macro(["a.xlsm" , "b.xlsm"] , ["Macro1", "Macro1Another"]) 
+                          
+                          """)
+            macro_names = [macro_names for _ in range(len(files))]
+
+    if macro_names:
+        fs = []
+        for file, macro_name in zip(files, macro_names):
+            fs.append(
+                File(
+                    file,
+                    root=root,
+                    macro_name=macro_name,
+                    approve=approve,
+                    dont_check=dont_check,
+                )
+            )
+
+    else:
+        fs = [File(x, root=root, approve=approve, dont_check=dont_check) for x in files]
+
     for a in fs:
         a.check()
 
@@ -116,25 +203,31 @@ def check_and_get_files(files, root=None, approve: bool = False, dont_check: boo
         yield File_
 
 
-def get_excel_app(approve=False):
+def get_excel_app(approve=False, visible=False):
     if excel_running():
         shut_excel(approve)
+    excel_app = win32com.client.DispatchEx("Excel.Application")
+    excel_app.Visible = int(visible)
 
-    excel_app = win32com.client.Dispatch("Excel.Application")
     return excel_app
 
 
-def refresh(files, root: Path | str | None = None, approve: bool = False):
+def refresh(
+    files, root: Path | str | None = None, approve: bool = False, visible=False, sleep=0
+):
     pythoncom.CoInitialize()
-    excel_app = None  # Initialize excel_app to None here
+    excel_app = None
 
     try:
-        excel_app = get_excel_app(approve)
-        for File_ in check_and_get_files(files, root, approve):
-            File_.refresh(excel_app)  # Pass excel_app to the refresh method
+        excel_app = get_excel_app(approve=approve, visible=visible)
+        for File_ in check_and_get_files(
+            files, macro_names=None, root=root, approve=approve
+        ):
+            File_.refresh(excel_app, sleep=sleep)
 
     except Exception as e:
         import traceback
+
         traceback.print_exc()
         print(f"An error occurred during refresh: {e}")
 
@@ -145,6 +238,44 @@ def refresh(files, root: Path | str | None = None, approve: bool = False):
                 print("Closing excel application!")
             except Exception as quit_error:
                 import traceback
+
+                traceback.print_exc()
+                print(f"Error while trying to close excel : {quit_error}")
+
+        pythoncom.CoUninitialize()
+
+
+def refresh_macro(
+    files,
+    macro_names: tuple,
+    root: Path | str | None = None,
+    approve: bool = False,
+    visible=False,
+    sleep=0,
+):
+    pythoncom.CoInitialize()
+    excel_app = None
+
+    try:
+        excel_app = get_excel_app(approve=approve, visible=visible)
+        for File_ in check_and_get_files(
+            files, macro_names=macro_names, root=root, approve=approve
+        ):
+            File_.refresh_macro(excel_app, sleep=sleep)
+    except Exception as e:
+        import traceback
+
+        traceback.print_exc()
+        print(f"An error occurred during refresh: {e}")
+
+    finally:
+        if excel_app is not None:
+            try:
+                excel_app.Quit()
+                print("Closing excel application!")
+            except Exception as quit_error:
+                import traceback
+
                 traceback.print_exc()
                 print(f"Error while trying to close excel : {quit_error}")
 
